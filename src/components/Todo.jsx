b95fa34238c8d885279f941255572a0d6b40afe4
Zairaxx <brandontsegai@gmail.com>
2025-10-30 15:03:19 +0100

    added Todo-component with props

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,8 @@
-const Todo = () => {
+const Todo = (props) => {
     
+    //Vi skickar en prop som heter "data" till denna komponent från TodoList.jsx. Där har vi namnet på vår todo
     return(
-        <li className="todo-item">Todo-item</li>
+        <li className="todo-item">{props.data}</li>
     )
 }
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,7 +6,9 @@ const TodoList = (props) => {
     return(<div className="todo-list">
         <h3>Todolist</h3>
         <ul>
-            {props.data.map(todo => <li>{todo}</li>)}
+            {/* För varje todo i arrayen, returnera en Todo-komponent och skicka med namnet som props */}
+            {/* Detta kommer ge oss tre Todo-komponenter, med varsin prop - Städa, Plugga React,   */}
+            {props.data.map(todo => <Todo data={todo} />)}
         </ul>
     </div>)
 
